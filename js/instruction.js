033af349a59cf54d8f3020467a99fe035ab5f04c
Luna <luch0005@stud.ek.dk>
2025-11-03 15:42:09 +0100

    Farver ændret så det passer til styletile, første uddrag af indberet tilføjet

--- js/instruction.js
+++ js/instruction.js
@@ -4,16 +4,22 @@ document.querySelector("#mund").addEventListener("click", mundF);
 
 function mundF() {
   console.log("mundF er trykket paa");
+
+  document.querySelector(".info-text h2").textContent = "Mund";
+  document.querySelector(".info-text p").textContent =
+    "Hamsteret bruger sin mund til at opbevare ting. Den kan f.eks. opbevare det den har røvet eller smugle ting ind/ud af landet ved at opbevare det i dens store mund.";
 }
 
 document.querySelector("#kloer").addEventListener("click", kloerF);
 
 function kloerF() {
   console.log("kloerF er trykket paa");
+  document.querySelector(".info-text h2").textContent = "Klør";
 }
 
 document.querySelector("#mave").addEventListener("click", maveF);
 
 function maveF() {
   console.log("maveF er trykket paa");
+  document.querySelector(".info-text h2").textContent = "Mave";
 }
